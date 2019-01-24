491ffabda4fdf8e614085e4381cf19c8e241a24c
Alex Ng <atn118@ucsd.edu>
2019-01-24 14:44:58 -0800

    fixed the node

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -3,17 +3,19 @@
  * GET home page.
  */
 
-exports.view = function(req, res){
-	/*if (userName != null) {
-		res.render('index', {
-			'name': userName,
-		});
-	}*/
-	//else {
-  res.render('index', {
-  	'name': 'World',
+exports.view = function(req, res) {
+	var nameToShow = req.params.userName;
+	if (nameToShow === undefined) {
+		nameToShow = 'World';
+	}
+	console.log("Name is " + nameToShow);
+	res.render('index', {
+  	'name': nameToShow,
   });
-	//}
+	/*res.render('index', {
+  	'name': '',
+  })*/
+  
 };
 
 /*
