d65ec16034722af51a777879498a9eb337d8d738
Alex Ng <atn118@ucsd.edu>
2019-01-24 10:43:23 -0800

    node stuff

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,22 @@
  */
 
 exports.view = function(req, res){
+	/*if (userName != null) {
+		res.render('index', {
+			'name': userName,
+		});
+	}*/
+	//else {
   res.render('index', {
-  	'name': '',
+  	'name': 'World',
   });
+	//}
 };
+
+/*
+exports.get('hello/:userName', function(request, response, next) {
+	var username = request.params.userName;
+	res.render('index', {
+		'name': userName,
+	});
+});*/
